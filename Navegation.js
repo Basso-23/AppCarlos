35b2c0b3e088942df21d28ea3476aa19dd91e5d2
Carlos Baso <virtual10pr10@gmail.com>
2022-09-15 14:09:06 -0500

    Navegation updated

--- Navegation.js
+++ Navegation.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import {
   Text,
   View,
@@ -9,67 +9,74 @@ import {
   ImageBackground,
   StatusBar,
   SafeAreaView,
-} from "react-native";
+} from 'react-native';
 import {
   addMultipleGifs,
   deleteAllGifs,
   getSingleGif,
   saveTEXTfile,
   readTEXTfile,
-} from "./screens/TextFile";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import Header from "./screens/Inicio/HeaderWhite";
-import { Dimensions } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+} from './screens/TextFile';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Header from './screens/Inicio/HeaderWhite';
+import { Dimensions } from 'react-native';
 
 //screens
-import Inicio from "./screens/Inicio/A-Inicio";
-import Web from "./screens/Web/A-Web";
-import Cuenta from "./screens/Cuenta/A-Cuenta";
-import Eventos from "./screens/Eventos/A-Eventos";
-//Stacks
-import NegociosStack from "./screens/Negocios/NegociosStack";
+import Inicio from './screens/Inicio/A-Inicio';
+import Negocios from './screens/Negocios/A-Negocios';
+import Web from './screens/Web/A-Web';
+import Cuenta from './screens/Cuenta/A-Cuenta';
+import Eventos from './screens/Eventos/A-Eventos';
+import Cuenta2 from './screens/Cuenta/A-Cuenta2';
 
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { FontAwesome } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
-let dimensionsH = Dimensions.get("window").height;
+let dimensionsH = Dimensions.get('window').height;
 
 function MyTabs() {
-  let [state, setState] = useState({
+  const [state, setState] = useState({
     url: null,
-    inicio: "inicio",
-    negocios: "negocios",
-    web: "web",
+    inicio: 'Inicio',
+    negocios: 'Negocios',
+    web: 'Web',
   });
-  const navigation = useNavigation();
-  const goToMap = () => {
-    navigation.navigate("Negocios", { screen: "Negocio" });
-  };
+
+  useEffect(() => {
+    let prueba = readTEXTfile();
+    prueba.then((value) => {
+      state.language = value;
+      if (value == 'chino') {
+        setState({
+          ...state,
+          inicio: '家',
+          negocios: '餐馆',
+          web: '网络',
+        });
+      }
+    });
+  }, []);
+
   return (
     <Tab.Navigator
       screenOptions={{
         headerStyle: { height: 75 },
-        tabBarActiveTintColor: "#fedc00",
+        tabBarActiveTintColor: '#ffcc00',
         tabBarStyle: {
-          backgroundColor: "black",
+          backgroundColor: 'black',
           height: dimensionsH / 14,
           paddingTop: 0,
           borderTopWidth: 0,
         },
-        headerTitle: "",
-      }}
-      //label de los iconos del BottomTab
-      tabBarOptions={{
-        labelStyle: { paddingBottom: 0 },
-      }}
-    >
+        headerTitle: '',
+      }}>
       <Tab.Screen
-        name="Home"
+        name={state.inicio}
         component={Inicio}
         options={{
           tabBarIcon: ({ color, size }) => (
@@ -80,22 +87,19 @@ function MyTabs() {
       />
 
       <Tab.Screen
-        name="Negocios"
-        component={NegociosStack}
+        name={state.negocios}
+        component={Negocios}
         options={{
           tabBarIcon: ({ color, size }) => (
             <FontAwesome name="list" size={17} color={color} />
           ),
           headerShown: false,
-          tabBarButton: (props) => (
-            <TouchableOpacity {...props} onPress={goToMap} />
-          ),
         }}
       />
 
       <Tab.Screen
-        initialParams={{ url: "https://pidepaya.com/" }}
-        name="Web"
+        initialParams={{ url: 'https://pidepaya.com/' }}
+        name={state.web}
         component={Web}
         options={{
           tabBarIcon: ({ color, size }) => (
@@ -130,10 +134,106 @@ function MyTabs() {
   );
 }
 
+function MyTabs2() {
+  const [state, setState] = useState({
+    url: null,
+    inicio: 'Inicio',
+    negocios: 'Negocios',
+    web: 'Web',
+  });
+
+  useEffect(() => {
+    let prueba = readTEXTfile();
+    prueba.then((value) => {
+      state.language = value;
+      if (value == 'chino') {
+        setState({
+          ...state,
+          inicio: '家',
+          negocios: '餐馆',
+          web: '网络',
+        });
+      }
+    });
+  }, []);
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerStyle: { height: 75 },
+        tabBarActiveTintColor: '#ffcc00',
+        tabBarStyle: {
+          backgroundColor: 'black',
+          height: dimensionsH / 14,
+          paddingTop: 0,
+          borderTopWidth: 0,
+        },
+        headerTitle: '',
+      }}>
+      <Tab.Screen
+        name={state.inicio}
+        component={Inicio}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="home" color={color} size={22} />
+          ),
+          headerBackground: () => <Header />,
+        }}
+      />
+
+      <Tab.Screen
+        name={state.negocios}
+        component={Negocios}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="list" size={17} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+
+      <Tab.Screen
+        initialParams={{ url: 'https://pidepaya.com/' }}
+        name={state.web}
+        component={Web}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="bookmark" size={20} color={color} />
+          ),
+          headerBackground: () => <Header />,
+        }}
+      />
+
+      <Tab.Screen
+        name="Eventos"
+        component={Eventos}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="ticket" size={20} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+
+      <Tab.Screen
+        name="Cuenta"
+        component={Cuenta2}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="user" size={20} color={color} />
+          ),
+          headerBackground: () => <Header />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
 export default function Navigation() {
+ 
   return (
     <NavigationContainer>
-      <MyTabs />
+      <MyTabs2/>
     </NavigationContainer>
   );
 }
---
+++ screens/Cuenta/A-Cuenta2.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  ImageBackground,
+  StatusBar,
+  Dimensions,
+} from 'react-native';
+
+
+
+const Cuenta2 = () => {
+  
+  
+
+    return (
+      <View>
+     <Text>Cuenta2</Text>
+     </View>
+    );
+  };
+export default Cuenta2;
